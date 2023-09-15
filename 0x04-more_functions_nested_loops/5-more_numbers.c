d22f2706bfa74bda9617814adcff6d085d4ca634
Oke Abdulakeem <du2okeakeem@gmail.com>
2023-09-15 03:02:44 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-more_numbers.c -o 5-more_n

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * more_numbers - print 10 x the numbers, from 0 to 14
+ * more_numbers - print 10 times the numbers, from 0 to 14
  * followed by a new line
  */
+
 void more_numbers(void)
 {
 	int i, j;
@@ -12,8 +13,8 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
